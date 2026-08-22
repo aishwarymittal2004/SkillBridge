a8ff4340222a5c219f45f89d06581cbec9e3c3d5
Aishwary Mittal <aishwarymittal2004@gmail.com>
2026-08-23 00:18:49 +0530

    fix neon database connection

--- backend/app/database/db.py
+++ backend/app/database/db.py
@@ -1,9 +1,14 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:aish123@localhost:5432/jobbridge"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
